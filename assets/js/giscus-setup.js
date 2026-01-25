5778738c219cb8189ad3e68cc2f12eb13f93da64
jhyun1202 <jhyun1202@users.noreply.github.com>
2026-01-25 11:56:16 +0000

    Deploying to gh-pages from @ jhyun1202/jhyun1202.github.io@49e2b123e9153bb2a7c9024cfa5d98110617e4c1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jhyun1202/al-folio",
+    "data-repo": "jhyun1202/jhyun1202.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
